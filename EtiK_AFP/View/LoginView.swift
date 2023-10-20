df83d7ba76d1f0136b858199b5810a2caa2e1f25
Ginley <annerose_ginley@outlook.fr>
2023-10-20 15:09:24 +0200

    Vue Log In

---
+++ EtiK_AFP/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  EtiK_AFP
+//
+//  Created by apprenant62 on 20/10/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ EtiK_AFP/View/ModifCompteView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifCompteView.swift
+//  EtiK_AFP
+//
+//  Created by apprenant62 on 20/10/2023.
+//
+
+import SwiftUI
+
+struct ModifCompteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifCompteView()
+}
