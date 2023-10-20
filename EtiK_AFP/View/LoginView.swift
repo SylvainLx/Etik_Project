261cd36abeaa8d92bfd25be408895e1814a07f94
Ginley <annerose_ginley@outlook.fr>
2023-10-20 15:10:28 +0200

    Modification de compte View

--- EtiK_AFP/View/LoginView.swift
+++ EtiK_AFP/View/LoginView.swift
@@ -8,8 +8,105 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var pseudo: String = ""
+    @State private var passWord: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+        
+            Circle()
+                .fill(.beige)
+                .frame(width: 475, height: 475)
+                .offset(CGSize(width: 0, height: -350))
+            Button{
+                
+            } label: {
+                Image("ProfilPic")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .offset(CGSize(width: 0, height: -150))
+                
+            }
+            VStack {
+                TextField("Pseudo", text: $pseudo)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 296)
+                
+                TextField("Mot de passe", text: $passWord)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(8)
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(.marron)
+                            .frame(width: 300, height: 50)
+                        Text("Connexion")
+                            .foregroundStyle(.white)
+                            .font(.custom("Italiana", size: 20))
+                    }
+                }
+                .padding(8)
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.black, lineWidth: 4)
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 50)
+                            
+                        Text("Inscription")
+                            .foregroundStyle(.black)
+                            .font(.custom("Italiana", size: 20))
+                        
+                        
+                    }
+                }
+                .padding(8)
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                        
+                            .foregroundColor(.black)
+                            .frame(width: 300, height: 50)
+                        HStack{
+                            Image(systemName: "apple.logo")
+                                .foregroundStyle(.white)
+                            
+                            Text("Sign in with apple")
+                                .foregroundStyle(.white)
+                                .font(.custom("Italiana", size: 20))
+                            
+                        }
+                    }
+                }
+                .padding(8)
+                
+                Button{
+                    
+                } label: {
+                        
+                        Text("Mot de passe oublié ?")
+                            .foregroundStyle(.blue)
+                            .font(.custom("Italiana", size: 20))
+                        
+                }
+                .padding(8)
+            }
+        }
     }
 }
 
--- EtiK_AFP/View/ModifCompteView.swift
+++ EtiK_AFP/View/ModifCompteView.swift
@@ -8,8 +8,104 @@
 import SwiftUI
 
 struct ModifCompteView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var passWord: String = ""
+    @State private var adresse: String = ""
+    @State private var postalCode: Int = 0000
+    @State private var ville: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(.beige)
+            
+            VStack{
+                Button{
+                } label: {
+                    Image("ProfilPic")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                
+                TextField("Nouveau Prénom", text: $firstName)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                
+                TextField("Nouveau Nom", text: $lastName)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                
+                TextField("Nouvelle adresse mail", text: $email)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                
+                TextField("Nouveau mot de passe", text: $passWord)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                
+                TextField("Nouvelle adresse", text: $adresse)
+                    .frame(width: 300)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 8)
+                
+                HStack{
+                    TextField("Code postal", value: $postalCode, formatter: NumberFormatter())
+                        .frame(width: 146)
+                        .textFieldStyle(.roundedBorder)
+                        .multilineTextAlignment(.center)
+                        .padding([.top, .leading], 8)
+                    
+                    TextField("Nouvelle ville", text: $ville)
+                        .frame(width: 146)
+                        .textFieldStyle(.roundedBorder)
+                        .multilineTextAlignment(.center)
+                        .padding([.top, .leading, .trailing], 8)
+                }
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(.marron)
+                            .frame(width: 300, height: 50)
+                        Text("Enregistrer les modifications")
+                            .foregroundStyle(.white)
+                            .font(.custom("Italiana", size: 20))
+                    }
+                }
+                .padding(8)
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(.red)
+                            .frame(width: 300, height: 50)
+                        Text("Annuler")
+                            .foregroundStyle(.white)
+                            .font(.custom("Italiana", size: 20))
+                    }
+                }
+            }
+            .padding(.bottom, 100)
+            
+        }
+        .padding(.top, 100.0)
     }
 }
 
