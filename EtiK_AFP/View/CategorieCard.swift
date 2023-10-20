08631bebe9602e55610d702947e77c3088413a4d
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-20 12:03:05 +0200

    Add SearchBar and CategoryCard

---
+++ EtiK_AFP/View/CategorieCard.swift
@@ -0,0 +1,18 @@
+//
+//  CategorieCard.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 20/10/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ EtiK_AFP/View/Searchtab.swift
@@ -0,0 +1,26 @@
+//
+//  Search Bar.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 20/10/2023.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var searchText: String
+
+    var body: some View {
+        NavigationStack {
+        }
+        .searchable(text: $searchText, prompt: "Veuillez taper votre recherche ici")
+
+        }
+    }
+
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(searchText: .constant(""))
+    }
+}
