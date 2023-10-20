e73c7c5f5cd9ca32639d995e640751ba6f388a7c
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-20 15:22:38 +0200

    add ReturnArticleView

---
+++ EtiK_AFP/View/ReturnArticleView.swift
@@ -0,0 +1,18 @@
+//
+//  ReturnArticleView.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+struct ReturnArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReturnArticleView()
+}
