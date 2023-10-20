31490d8b1a4779706da3bcb5167df33984196800
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-20 14:05:20 +0200

    Thematic cart

--- EtiK_AFP/View/Thematiquecard.swift
+++ EtiK_AFP/View/Thematiquecard.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct Thematiquecard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Rectangle()
+                .frame(height: 150)
+                .foregroundColor(.black)
+            
+            ZStack {
+                Image("thematiqueimg2")
+                    .resizable()
+                    .frame(width: 250, height: 150)
+                .padding(.top, 0)
+                Image("thematiqueimg4")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .offset(x:-30)
+                    .padding(.top, 40)
+            }
+            
+            HStack {
+                Image("thematiqueimg1")
+                    .resizable()
+                    .frame(width: 100, height: 140)
+                    .padding(.top, 10)
+                    .offset(x:-30)
+
+                Image("thematiqueimg3")
+                    .resizable()
+                    .frame(width: 250, height: 180)
+                    .padding(.top, -30)
+                    .offset(x:99)
+            }
+        }
     }
 }
 
