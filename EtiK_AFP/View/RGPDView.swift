d1a99bf2f1d4b57f339d444f426a60c16b35dc4e
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-20 13:54:59 +0200

    add rgpdview

---
+++ EtiK_AFP/View/RGPDView.swift
@@ -0,0 +1,18 @@
+//
+//  RGPDView.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+struct RGPDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RGPDView()
+}
