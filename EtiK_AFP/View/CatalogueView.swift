98f2b1f0e3e2397ba4f10a29e44fa00bb58c2e84
Emilien <emiliengir@gmail.com>
2023-10-20 15:20:43 +0200

    CatalogueView

--- EtiK_AFP/View/CatalogueView.swift
+++ EtiK_AFP/View/CatalogueView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct CatalogueView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        Text("Catalogue")
+            .font(.custom("Italianno", size: 50))
+            .padding(.vertical, -10)
+        
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10) {
+                ForEach(1...7, id: \.self) { _ in
+                    CardProduit()
+                }
+            }
+        }
     }
 }
 
