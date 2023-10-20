f246c3f2e714deeecef86e5463f6635e83b6474d
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-20 11:35:01 +0200

    Initial Commit

---
+++ EtiK_AFP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EtiK_AFP/EtiK_AFPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtiK_AFPApp.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EtiK_AFPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
