bcec13bd75ad8ea76977124c672df67ab7051799
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-23 13:52:25 +0200

    add command/return view and command/returnDetail View with a chat open btn

---
+++ EtiK_AFP/Helpers/CommandReturnCard.swift
@@ -0,0 +1,18 @@
+//
+//  CommandReturnCard.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 23/10/2023.
+//
+
+import SwiftUI
+
+struct CommandReturnCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommandReturnCard()
+}
