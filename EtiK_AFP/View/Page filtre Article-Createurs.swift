8cd030f26cdfe46b714c7de26e4af74ff5b2d0d2
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-23 11:24:12 +0200

    Add Filtre Produits Createurs

---
+++ EtiK_AFP/View/Page filtre Article-Createurs.swift
@@ -0,0 +1,18 @@
+//
+//  Page filtre Article-Createurs.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 23/10/2023.
+//
+
+import SwiftUI
+
+struct Page_filtre_Article_Createurs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Page_filtre_Article_Createurs()
+}
