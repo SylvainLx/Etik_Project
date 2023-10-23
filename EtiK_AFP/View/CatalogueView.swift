a4e401e0f7cd210fa7b2151da01f2b4b7f5341de
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-23 09:58:05 +0200

    fav and notif params view

--- EtiK_AFP/View/CatalogueView.swift
+++ EtiK_AFP/View/CatalogueView.swift
@@ -22,6 +22,7 @@ struct CatalogueView: View {
                 }
             }
         }
+        .padding()
     }
 }
 
--- EtiK_AFP/View/NotifParamView.swift
+++ EtiK_AFP/View/NotifParamView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct NotifParamView: View {
+    
+    @State private var newCreator: Bool = false
+    @State private var newProduct: Bool = true
+    @State private var commandFollow: Bool = false
+    @State private var eventFollow: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading) {
+            Text("Paramètres de notifications")
+                .font(.custom("Italiana", size: 24))
+                .padding()
+                .padding(.bottom, 16)
+            VStack(spacing: 32) {
+                Toggle(isOn: $newCreator) {
+                    Text("Nouveau Créateur")
+                }
+                Toggle(isOn: $newProduct) {
+                    Text("Nouveau produit d'un créateur suivi")
+                }
+                Toggle(isOn: $commandFollow) {
+                    Text("Suivis de commande")
+                }
+                Toggle(isOn: $eventFollow) {
+                    Text("Evènements à venir")
+                }
+            }
+            Spacer()
+        }.padding()
     }
 }
 
