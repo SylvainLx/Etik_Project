67517bdb93836b8dee25a66d6490673bf7a4cb4d
Ginley <annerose_ginley@outlook.fr>
2023-10-23 16:56:25 +0200

    Detail Event (mis en page en cours et paramètre en attente)

---
+++ EtiK_AFP/View/ParametreView.swift
@@ -0,0 +1,74 @@
+//
+//  ParametreView.swift
+//  EtiK_AFP
+//
+//  Created by apprenant62 on 23/10/2023.
+//
+
+import SwiftUI
+
+struct ParametreView: View {
+    
+    var body: some View {
+        
+        NavigationStack{
+            List{
+                Button{
+                    
+                }label: {
+                    Text("Général")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Sécurité et privacy")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Activités")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Commande et Paiment")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Vérification")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Support")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Affichage")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Information mise à jour")
+                }
+                Button{
+                    
+                }label: {
+                    Text("Langage")
+                }
+                Button{
+                    
+                }label: {
+                    Text("A propos")
+                }
+            }
+            .navigationTitle("Paramètre")
+        }
+    }
+}
+
+#Preview {
+    ParametreView()
+}
