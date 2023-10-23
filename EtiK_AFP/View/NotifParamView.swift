7e8df6235764844ec7105d6fc7a0c09c4b570b7a
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-23 09:57:45 +0200

    fav view and notif params view

---
+++ EtiK_AFP/View/NotifParamView.swift
@@ -0,0 +1,18 @@
+//
+//  NotifParamView.swift
+//  EtiK_AFP
+//
+//  Created by Sylvain Leguay on 23/10/2023.
+//
+
+import SwiftUI
+
+struct NotifParamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotifParamView()
+}
