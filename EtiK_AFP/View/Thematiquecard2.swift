717ff6c6302e8eda017fcb59b59facbe3179fde6
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-23 16:08:58 +0200

    Ajout Thematique 2 et 3

---
+++ EtiK_AFP/View/Thematiquecard2.swift
@@ -0,0 +1,18 @@
+//
+//  Thematiquecard2.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 23/10/2023.
+//
+
+import SwiftUI
+
+struct Thematiquecard2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Thematiquecard2()
+}
---
+++ EtiK_AFP/View/Thematiquecard3.swift
@@ -0,0 +1,18 @@
+//
+//  Thematiquecard3.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 23/10/2023.
+//
+
+import SwiftUI
+
+struct Thematiquecard3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Thematiquecard3()
+}
