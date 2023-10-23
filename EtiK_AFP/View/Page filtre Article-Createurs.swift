b681cf66bfc79be37720c104b80de84b95db5bad
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-23 11:24:54 +0200

    Add Filter page Product -Creators

--- EtiK_AFP/View/Page filtre Article-Createurs.swift
+++ EtiK_AFP/View/Page filtre Article-Createurs.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct Page_filtre_Article_Createurs: View {
+    @State private var message: String?
+    @State private var searchText = ""
+    let data: [String] = (1...2).map { "Élément \($0)" }
+    let columns = [
+        GridItem(.flexible(),spacing: 0),
+        GridItem(.flexible(),spacing: 0)
+    ]
+    
+    @State private var filteredProduits: [String] = ["Apple", "Banana", "Cherry", "Date", "Fig", "Grapes"]
+    @State private var filteredCreateurs: [String] = ["Pomme", "Banane", "Cerise", "Date", "Figue", "Raisin"]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            
+            VStack(alignment: .center){
+                
+                HStack (spacing: -2){
+                    
+                    Button(action: {
+                        message = "Produits"
+                    }) {
+                        Text("Produits")
+                            .frame(maxWidth: .infinity)
+                            .padding(10)
+                            .background(Color.white)
+                        
+                            .foregroundColor(.black)
+                    }
+                    .border(Color.black, width: 2)
+                    
+                    Button(action: {
+                        message = "Créateurs"
+                    }) {
+                        Text("Créateurs")
+                            .frame(maxWidth: .infinity)
+                            .font(.headline)
+                            .padding(10)
+                            .background(Color.white)
+                            .foregroundColor(.black)
+                        
+                    }
+                    .border(Color.black, width: 2)
+                }
+                ScrollView{
+                    LazyVGrid(columns: columns,spacing: 0) {
+                        ForEach(data, id: \.self) { item in
+                            ZStack{
+                                Rectangle()
+                                    .frame(height: 620)
+                                    .background()
+                                    .opacity(0)
+                                VStack {
+                                    
+                                    if let message = message {
+                                        Text(message )
+                                        
+                                        Text(item)
+                                            .font(.title)
+                                            .padding()
+                                        
+                                        
+                                        if message == "Produits" {
+                                            List(filteredProduits.filter {
+                                                searchText.isEmpty ? true : $0.localizedCaseInsensitiveContains(searchText)
+                                            }, id: \.self) { item in
+                                                Text(item)
+                                            }
+                                        } else if message == "Créateurs" {
+                                            List(filteredCreateurs.filter {
+                                                searchText.isEmpty ? true : $0.localizedCaseInsensitiveContains(searchText)
+                                            }, id: \.self) { item in
+                                                Text(item)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .searchable(text: $searchText, prompt: "Veuillez saisir votre recherche ici")
+        }
     }
 }
+    #Preview {
+        Page_filtre_Article_Createurs();
+    }
 
-#Preview {
-    Page_filtre_Article_Createurs()
-}
