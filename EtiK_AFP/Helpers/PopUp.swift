aafe484724e17bc2375a57c82ecab25dde94febe
Emilien <emiliengir@gmail.com>
2023-10-25 12:19:30 +0200

    Modifs profils

---
+++ EtiK_AFP/Helpers/PopUp.swift
@@ -0,0 +1,18 @@
+//
+//  PopUp.swift
+//  EtiK_AFP
+//
+//  Created by Émilien Giraud on 25/10/2023.
+//
+
+import SwiftUI
+
+struct PopUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUp()
+}
