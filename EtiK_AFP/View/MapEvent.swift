bb4fb73e46df4a40d922a9675983f166747fe887
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-25 11:07:59 +0200

    Ajout de la vue Map

---
+++ EtiK_AFP/View/MapEvent.swift
@@ -0,0 +1,18 @@
+//
+//  MapEvent.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 24/10/2023.
+//
+
+import SwiftUI
+
+struct MapEvent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapEvent()
+}
---
+++ EtiK_AFP/View/Map_struc.swift
@@ -0,0 +1,8 @@
+//
+//  Map_struc.swift
+//  EtiK_AFP
+//
+//  Created by apprenant48 on 25/10/2023.
+//
+
+import Foundation
