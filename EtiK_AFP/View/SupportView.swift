6771e82e20d8a2de2d160c66d4681ac64ca1f036
Ginley <annerose_ginley@outlook.fr>
2023-10-26 16:47:29 +0200

    écrant support corrigé

--- EtiK_AFP/View/SupportView.swift
+++ EtiK_AFP/View/SupportView.swift
@@ -10,28 +10,57 @@ import SwiftUI
 struct SupportView: View {
     
     let chatSupport = Chat.sampleChat
+    let chatContent: Chat = Chat.sampleChat[1]
     
     var body: some View {
         
         NavigationStack {
             ZStack {
                 
-                
-                
                 VStack{
-                    ForEach (chatSupport) { chat in
-                        
-                        NavigationLink(destination: {
-                            ChatView(chat: chat)
-                        }) {
-                            ChatRow(chat: chat)
-                            
-                            
-                        }
-                        .padding(.horizontal)
-                        
-                        
-                    }
+                    HStack(spacing: 20) {
+
+                    Image(chatContent.person.imgString)
+                             .resizable()
+                            .frame(width: 70, height: 70)
+                             .clipShape(Circle())
+
+                         ZStack {
+                             VStack(alignment: .leading, spacing: 5) {
+                                 HStack {
+                                     Text(chatContent.person.name)
+                                         .bold()
+                                         .foregroundColor(.white)
+
+                                     Spacer()
+
+                                     Text(chatContent.messages.last?.date.descriptiveString() ?? "")
+                                         .foregroundColor(.white)
+                                       
+
+                                 }
+
+                                 HStack {
+
+                                     Text(chatContent.messages.last?.text ?? "")
+                                         .bold()
+                                         .foregroundColor(.white)
+                                         .lineLimit(2)
+                                         .frame(height: 50, alignment: .top)
+                                         .frame(maxWidth: .infinity, alignment: .leading)
+                                         .multilineTextAlignment(.leading)
+                                         .padding(.trailing, 40)
+                                     }
+                             }
+                       Circle()
+                                 .foregroundColor(chatContent .hasUnreadMessage ? (Color(red: 0.747, green: 0.668, blue: 0.447)): .clear)
+
+                                 .frame(width: 18, height: 18)
+                                 .frame(maxWidth: .infinity, alignment: .trailing)
+
+                         }
+                     }
+                     .frame(height: 80 )
                     Spacer()
                 }
                 
