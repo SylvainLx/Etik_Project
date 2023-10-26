6d93b739dc9039a89a462f88639e29dacb8366bc
Ginley <annerose_ginley@outlook.fr>
2023-10-26 14:48:58 +0200

    Début page Support

--- EtiK_AFP/View/SupportView.swift
+++ EtiK_AFP/View/SupportView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct SupportView: View {
+    
+    let chatSupport = Chat.sampleChat
+    
     var body: some View {
-        Text("Support")
+        
+        NavigationStack {
+            ZStack {
+                
+                
+                
+                VStack{
+                    ForEach (chatSupport) { chat in
+                        
+                        NavigationLink(destination: {
+                            ChatView(chat: chat)
+                        }) {
+                            ChatRow(chat: chat)
+                            
+                            
+                        }
+                        .padding(.horizontal)
+                        
+                        
+                    }
+                    Spacer()
+                }
+                
+                
+                .navigationTitle("Posez votre question")
+                .navigationBarItems(trailing: Button(action: {}) {
+                    Image(systemName: "square.and.pencil")
+                })
+                
+                
+                
+            }
+        }
+        
     }
 }
 
