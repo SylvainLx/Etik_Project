e85681f7da5a2c115d82e781f1e38466b540d164
Ginley <annerose_ginley@outlook.fr>
2023-10-26 12:28:04 +0200

    Régulation EU

--- EtiK_AFP/View/EURegulationView.swift
+++ EtiK_AFP/View/EURegulationView.swift
@@ -11,8 +11,83 @@ struct EURegulationView: View {
     
     var body: some View {
         
-        Text("Régulation")
-        
+        VStack(spacing : 16){
+            
+            TitleCard(title: "Régulation UE")
+            ScrollView{
+                
+                Text("""
+La Commission européenne supprime les obstacles en ligne afin que les citoyens puissent accéder pleinement à tous les biens et services offerts en ligne par les entreprises de l’UE.
+
+L’UE a permis aux consommateurs européens de faire des achats en ligne, peu importe où ils se trouvent dans l’UE, plus facile et plus sûr. Pour exploiter pleinement le potentiel du commerce électronique, l’UE a travaillé sur:
+
+la directive révisée sur les services de paiement et les nouvelles règles relatives aux services de livraison transfrontière de colis qui sont déjà en vigueur;
+de nouvelles règles pour mettre fin au blocage géographique injustifié;
+révision des règles de protection des consommateurs;
+nouvelles règles en matière de TVA pour la vente en ligne de biens et de services.
+
+De nouvelles règles pour mettre fin au blocage géographique injustifié dans l’UE
+
+Le blocage géographique nous empêche d’acheter à partir d’un site web basé dans un autre État membre de l’UE. Cela crée des obstacles pour les consommateurs dans les achats transfrontaliers.
+
+Que fait la Commission à ce sujet?
+
+La Commission a annoncé des règles à l’échelle de l’UE pour mettre fin à la discrimination en ligne fondée sur la nationalité ou le lieu de résidence. Elle introduit également des règles pour nous assurer que nous ne nous heurtons plus à des obstacles injustifiés tels que la réacheminement vers des sites Web spécifiques à un pays, ou l’obligation de payer par carte de débit ou de crédit d’un pays donné.
+
+Ces règles contribueront à garantir que les vendeurs en ligne traitent tous les consommateurs de l’UE sur un pied d’égalité, quel que soit l’endroit où ils choisissent de faire leurs achats.
+
+En savoir plus sur le géoblocage en lisant le Q&A, disponible dans les langues suivantes:
+
+BG, CS, DA, DE, EL, EN, ES, ET, FI, FR, HR, HU, IT, LT, LV, MT, NL, PL, PT, RO, SK, SL, SV
+
+Réduire les livraisons transfrontalières de colis
+
+Les prix de livraison transfrontière des colis sont en moyenne de 3 à 5 fois plus élevés que les prix de livraison nationaux pour tous les produits. Environ 62 pour cent des entreprises qui souhaitent vendre en ligne identifient les coûts de livraison élevés comme un problème. Il s’agit d’un obstacle au développement du commerce électronique transfrontalier.
+
+De nouvelles règles relatives aux services de livraison transfrontière de colis en ligne facilitent la recherche du moyen le moins cher d’envoyer un colis d’un État membre à un autre.
+
+Qu’est-ce que les nouvelles règles changent?
+
+Il n’y a pas de plafond sur les prix de livraison, mais les entreprises doivent maintenant divulguer leurs prix clairement, de sorte que le consommateur peut facilement comparer les options. Les consommateurs peuvent consulter les prix de livraison de colis sur une page web dédiée sur le site web de la Commission européenne.
+
+Les autorités nationales recueilleront chaque année des informations auprès des entreprises de livraison de colis. Lorsque la livraison de colis est soumise à une obligation de service universel, les autorités réglementaires nationales seront tenues d’évaluer si les tarifs sont déraisonnablement élevés.
+
+Protéger vos droits en tant que consommateur en ligne
+
+Les règles à l’échelle de l’UE facilitent la protection des consommateurs en ligne par les États membres. Ces règles permettent de supprimer des sites ou des comptes de médias sociaux lorsque des escroqueries ont été identifiées. Ils permettent également de demander des informations aux fournisseurs de services internet ou aux banques, afin de retracer l’identité des commerçants en ligne malhonnêtes.
+
+La Commission a proposé de nouvelles règles pour les contrats numériques, qui commenceront à s’appliquer à la fin de 2021. Les règles créeront des droits plus clairs pour les consommateurs lors de l’accès au contenu numérique et aux services numériques.
+
+Par exemple, si les utilisateurs de contenu numérique reçoivent n’est pas comme convenu ou comme il est raisonnable de s’y attendre, ils ont des droits contractuels spécifiques. Ces droits s’appliquent également lorsque le consommateur a fourni des données à caractère personnel au professionnel sans payer de prix.
+
+La Commission a proposé un nouvel accord pour les consommateurs en 2018 afin de renforcer encore les droits des consommateurs en ligne. Le nouvel accord suggère que:
+
+les places de marché en ligne devront informer les consommateurs s’ils achètent auprès d’un professionnel ou d’un particulier, de sorte qu’ils aient connaissance de leurs droits en cas de problème;
+lorsque les consommateurs effectuent des recherches en ligne, ils seront clairement informés lorsqu’un résultat de recherche est payé par un professionnel et que les places de marché en ligne devront fournir des informations sur les principaux paramètres déterminant le classement des résultats;
+lorsque les consommateurs paient un service numérique, ils bénéficieront de certains droits d’information avec 14 jours pour résilier le contrat.
+De plus amples informations sur les droits des consommateurs et des détails sur les droits en tant que citoyen de l’UE sont disponibles dans toutes les langues de l’UE.
+
+Faciliter l’accès aux services audiovisuels
+
+Les Européens peuvent utiliser leurs abonnements en ligne à des films, à des manifestations sportives, à des livres électroniques, à des jeux vidéo ou à de la musique lorsqu’ils voyagent dans l’UE. La Commission s’emploie à créer un cadre moderne pour le droit d’auteur dans l’UE, qui permettra un accès transfrontière plus important au contenu en ligne en simplifiant la concession de licences pour les transmissions en ligne.
+
+De nouvelles règles de l’UE pour les médias audiovisuels ont été adoptées, adaptent les règles existantes à l’ère numérique et promeuvent les œuvres européennes et préservent la diversité culturelle. Ces règles ne sont pas encore en vigueur.
+
+Loi sur les services numériques
+
+La Commission a adopté une proposition de loi sur les services numériques (DSA). Parallèlement à une loi sur les marchés numériques, la présente proposition créera un espace numérique plus sûr et plus ouvert pour tous les utilisateurs et garantira des conditions de concurrence équitables pour les entreprises.
+
+La DSA a trois objectifs spécifiques:
+
+protéger plus efficacement les consommateurs et leurs droits fondamentaux en ligne;
+mettre en place un puissant cadre de transparence et de responsabilité pour les plateformes en ligne;
+favoriser l’innovation, la croissance et la compétitivité au sein du marché unique.
+Si la directive sur le commerce électronique demeure la pierre angulaire de la réglementation numérique, beaucoup a changé depuis son adoption il y a 20 ans. La DSA abordera ces changements et les défis qui les accompagnent, en particulier en ce qui concerne les intermédiaires en ligne.
+""")
+                .padding()
+                .font(.custom("LibreFranklin", size: 16))
+            }
+        }
     }
 }
 
