827de987cd99053a302433949af24edca936d720
Emilien <emiliengir@gmail.com>
2023-10-27 14:41:16 +0200

    Carte de fidélité Ok

---
+++ EtiK_AFP/Helpers/FidelityCard.swift
@@ -0,0 +1,18 @@
+//
+//  FidelityCard.swift
+//  EtiK_AFP
+//
+//  Created by Émilien Giraud on 27/10/2023.
+//
+
+import SwiftUI
+
+struct FidelityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FidelityCard()
+}
---
+++ EtiK_AFP/View/ProfilParams/FidelityView.swift
@@ -0,0 +1,18 @@
+//
+//  FidelityView.swift
+//  EtiK_AFP
+//
+//  Created by Émilien Giraud on 27/10/2023.
+//
+
+import SwiftUI
+
+struct FidelityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FidelityView()
+}
