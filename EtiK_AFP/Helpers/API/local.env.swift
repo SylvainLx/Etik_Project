3e13fdfe9c95a99bc74d8a657d4d320c2c91a46e
Ginley <annerose_ginley@outlook.fr>
2023-10-27 16:15:31 +0200

    Git.ignore et API mis à jours de mon coté

---
+++ EtiK_AFP/Helpers/API/local.env.swift
@@ -0,0 +1,10 @@
+//
+//  local.env.swift
+//  EtiK_AFP
+//
+//  Created by apprenant62 on 27/10/2023.
+//
+
+import Foundation
+
+let apiPersonalKey = "pat5zAXCHDzpm7sc0.fdd6ff89c5cef693dbf840303be63cd6ea287aef1178dfb6be087d0cad7a8b64"
